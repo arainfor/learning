e68fbef38e70ac5e8542a93a55380b2a1f58432a
Alan Rainford <arainfor@gmail.com>
2018-07-02 18:26:53 -0500

    Thanks for pointing out I can't spell

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-// Constructior Function
-function Circle(raidius) {
-    this.raidius = raidius;
-    this.draw = function() {
-        console.log("draw " + raidius);
-    }
-}
-
-const another = new Circle(1);
-another.draw();
-console.log("radius " + another.raidius);
-
 let objectNumber = {value:10};
 let valueNumber = 10;
 
-function increaseByReference(anthing) {
-    anthing.value++
+function increaseByReference(anything) {
+    anything.value++
 }
 
-function increaseByValue(anthing) {
-    anthing++
+function increaseByValue(anything) {
+    anything++
 }
+
 console.log(objectNumber);
 increaseByReference(objectNumber);
 console.log("Increased by Reference " + Object.keys(objectNumber) + " " + objectNumber.value);
 
 console.log(valueNumber);
-console.log(Object.name);
 increaseByValue(valueNumber);
 console.log("Increased by Value " + Object.keys(valueNumber) + " " + valueNumber);
